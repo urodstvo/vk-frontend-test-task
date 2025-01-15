c2ca89caa107fa363c63ee9a4068a92625e86c49
urodstvo <dead.kanji@mail.ru>
2025-01-15 17:23:32 +0300

    initial commit

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://api.github.com';
-
-// "https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}"
-
-export { useSearchRepositories } from './querys/searchRepositories';
